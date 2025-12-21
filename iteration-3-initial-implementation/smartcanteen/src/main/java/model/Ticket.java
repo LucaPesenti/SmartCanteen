f95123ecfe1c3344d6a0fff44bba3a1919579b66
LucaPesenti <lucapesenti2002@gmail.com>
2025-12-21 19:19:34 +0100

    Add domain model for UC2 meal service

---
+++ iteration-3-initial-implementation/smartcanteen/src/main/java/model/Ticket.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Ticket {
+
+    private String id;
+    private boolean valid;
+    private boolean used;
+    private Meal meal;
+
+    public Ticket(String id, boolean valid, Meal meal) {
+        this.id = id;
+        this.valid = valid;
+        this.used = false;
+        this.meal = meal;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public Meal getMeal() {
+        return meal;
+    }
+
+    public void markAsUsed() {
+        this.used = true;
+    }
+}
